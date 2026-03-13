2d5ce10b13fad9cd9f854e7f0bf24c9681fc37a4
potatocastle <66970j@yonsei.ac.kr>
2026-03-13 05:17:57 +0000

    계산기

---
+++ lec1/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 97;
+    printf("a %%d: %d\n", a);
+    printf("a %%i: %i\n", a);
+    printf("a %%c: %c\n", a);
+    printf("a %%f: %f\n", (float)a);
+    return 0;
+}
